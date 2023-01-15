06f88e47f4771c36cdeea85f98723e57d022f5cb
Tech support <120698756+thetechsupport@users.noreply.github.com>
2023-01-15 13:19:17 +0530

    scs

--- buten.com/js/random.js
+++
@@ -1,5 +0,0 @@
-function randomSite() {
-    var sites = ["https://resource.fyi/category/ui-libraries", "https://duuuuunk.com/straps/category/heros", "https://web3forms.com/#start", "https://graygrids.com/", "https://www.youtube.com/results?search_query=what+is+google+drive+api", "https://stackoverflow.com/questions/38320878/circle-button-css#:~:text=For%20a%20modern%20CSS%20approach,a%20tag)%20has%20no%20effect.", "https://colorhunt.co/", "https://mdbgo.com/#section-steps", "https://replit.com/@codewithharry/02-Day2-Application-of-Python#1.%20Jarvis%20Virtual%20Assistant/1.%20jarvis.py", "https://www.zoho.com/forms/", "https://app.netlify.com/", "https://daisyui.com/", "https://www.cricbuzz.com/profiles/370/ab-de-villiers", "https://www.cobratate.com/", "https://youtube.com/", "https://www.sportskeeda.com/", "https://indianexpress.com/", "https://www.nytimes.com/international/", "https://www.aajtak.in/", "https://www.lenskart.com/", "https://www.flipkart.com/", "https://www.amazon.in/",  ];
-    var randomNumber = Math.floor(Math.random() * sites.length);
-    window.location = sites[randomNumber];
-}
--- buten.com/js/video.js
+++
@@ -1,5 +0,0 @@
-function randomSite() {
-    var sites = ["https://resource.fyi/category/ui-libraries", "https://duuuuunk.com/straps/category/heros", "https://web3forms.com/#start", "https://graygrids.com/", "https://www.youtube.com/results?search_query=what+is+google+drive+api", "https://stackoverflow.com/questions/38320878/circle-button-css#:~:text=For%20a%20modern%20CSS%20approach,a%20tag)%20has%20no%20effect.",];
-    var randomNumber = Math.floor(Math.random() * sites.length);
-    window.location = sites[randomNumber];
-}
